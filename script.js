b688d244a0ce525abcdbb3b0c5c846b75fbc1513
Felipe Eugenio <justworkttk@gmail.com>
2025-04-21 02:12:37 -0300

    updating JS and HTML, avatar

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function toggleMode() {
     if(html.classList.contains('light')) {
        img.setAttribute('src', './assets/Avatarwhite.png')
     } else {
-        img.setAttribute('src', './assets/Avatar.png')
+        img.setAttribute('src', './assets/avatar.png')
     }
 
 
