f1b58a41849cffb797f63a7bd74cbf2646ed7c9f
Felipe Eugenio <justworkttk@gmail.com>
2025-04-21 02:42:51 -0300

    Fix image path for light mode avatar

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ function toggleMode() {
     //pegar a tag img
     const img = document.querySelector("#profile img")
     if(html.classList.contains('light')) {
-       img.setAttribute('src', './assets/Avatarwhite.png')
+       img.setAttribute('src', 'assets/Avatarwhite.png')
     } else {
-        img.setAttribute('src', './assets/avatar.png')
+        img.setAttribute('src', 'assets/avatar.png')
     }
 
 
