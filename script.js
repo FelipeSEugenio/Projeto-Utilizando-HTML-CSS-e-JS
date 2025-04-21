c2ee92515dee2849e0c738ce3df9442a48bc55be
Felipe Eugenio <justworkttk@gmail.com>
2025-04-21 02:25:35 -0300

    updating JS photo

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function toggleMode() {
     //pegar a tag img
     const img = document.querySelector("#profile img")
     if(html.classList.contains('light')) {
-       img.setAttribute('src', './assets/Avatarwhite.png')
+       img.setAttribute('src', './assets/avatarwhite.png')
     } else {
         img.setAttribute('src', './assets/avatar.png')
     }
