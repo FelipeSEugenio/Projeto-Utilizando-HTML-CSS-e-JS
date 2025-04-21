c56d2770b77be534a696e3c751bfdf56c63065da
Felipe Eugenio <justworkttk@gmail.com>
2025-04-21 00:33:05 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+function toggleMode() {
+    const html = document.documentElement
+    html.classList.toggle('light')
+    /* ou usar */
+    //pegar a tag img
+    const img = document.querySelector("#profile img")
+    if(html.classList.contains('light')) {
+       img.setAttribute('src', './assets/Avatarwhite.png')
+    } else {
+        img.setAttribute('src', './assets/Avatar.png')
+    }
+
+
+
+
+    // substituir a imagem
+
+}
